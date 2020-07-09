784bf7f1397342f547edcc1f29f2efe6b4265ba4
Nicholas Renninger <18428974+nicholasRenninger@users.noreply.github.com>
2020-07-09 04:16:39 -0600

    can now auto-run all of the notebooks as e2e test cases

--- run_all_nbs.py
+++ run_all_nbs.py
@@ -13,8 +13,10 @@ for notebook_filename in glob.glob("*.ipynb"):
 
     with open(notebook_filename) as f:
         nb = nbformat.read(f, as_version=4)
+
+        print('Running:', notebook_filename)
         ep = ExecutePreprocessor(timeout=600)
-        ep.preprocess(nb, {'metadata': {'path': 'notebooks/'}})
+        ep.preprocess(nb, {'metadata': {'path': '.'}})
 
         with open(notebook_filename, 'w+', encoding='utf-8') as f:
             nbformat.write(nb, f)
